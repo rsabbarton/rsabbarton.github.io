46507b4b22290a03b8c8d2f162ba5a9276e8a2ca
rsabbarton <richard.sabbarton@gmail.com>
2024-04-03 11:34:50 +0000

    Testing GH API

--- static/js/project.js
+++ static/js/project.js
@@ -10,6 +10,34 @@ class Project {
     render(options) {
         console.log(options)
         this.options = options
-        
+
+        this.container = document.getElementById(config.containerId)
+        console.log(this.container)
+        this.container.innerHTML = ""
+        this.apiUrl = `https://api.github.com/repos/${this.config.githubOrg}/${this.config.githubRepo}`
+        get(apiUrl)
+        .then(response=>{
+            console.log(response)
+            this.container.innerHTML = response
+        })
+
+    }
+
+    get(url){
+        return new Promise((resolve, reject)=>{
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", url);
+            xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            xhr.send(null);
+            xhr.onreadystatechange = (e) => {
+              if(e.currentTarget.readyState == 4){
+                resolve(JSON.parse(xhr.responseText));
+              }
+            }
+            xhr.onerror = (e) => {
+                console.log(e)
+                reject(e);
+            }
+        })
     }
 }
